903d6c4e9248708957ab2c7446283f8c68ffd3cb
Sukkin Pang <skp@skpang.co.uk>
2025-07-31 21:26:36 +0100

    Update Squareline Studio

---
+++ 480x480_Squareline_studio/SquareLine_Project_events.py
@@ -0,0 +1,28 @@
+
+def start_event(event_struct):
+    return
+
+
+def stop_event(event_struct):
+    return
+
+
+def yellow_pressed(event_struct):
+    return
+
+
+def blue_pressed(event_struct):
+    return
+
+
+def brightnessInc(event_struct):
+    return
+
+
+def brightnessDec(event_struct):
+    return
+
+
+def clear_pressed(event_struct):
+    return
+
